4ce8782ec6f1c1ca78a272e0abd74ac9a849c03f
Sunjida Akter <133886845+sunjida-akter321@users.noreply.github.com>
2023-05-18 23:55:24 +0600

    Add files via upload

---
+++ Area of rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+int a,b,area;
+printf("Please Enter the value of width:");
+scanf("%d",&a);
+printf("Please Enter the value of Length:");
+scanf("%d",&b);
+area=a*b;
+printf("The value of Area:%d",area);
+}
